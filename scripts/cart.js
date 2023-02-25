c72eb15e2d83ca04bb73d6a0c5f13c9ca2d257a1
Ankush N Murthy <ankushn566@gmail.com>
2023-02-25 09:46:29 +0530

    day three tasks completed

---
+++ scripts/cart.js
@@ -0,0 +1,102 @@
+let cartItems = JSON.parse(localStorage.getItem('cart')) || []
+let left = document.getElementById('left')
+let totalId = document.getElementById('total')
+window.onload = function (){
+    display(cartItems)
+    let total = calTotal(cartItems);
+    totalId.innerText = `$${total}`
+}
+
+function display(data){
+    left.innerHTML  = null;
+    data.map((e,i)=>{
+         let div = document.createElement('div')
+         let imageDiv = document.createElement('div')
+         let textDiv = document.createElement('div')
+         let buttonDiv = document.createElement('div')
+
+         let image = document.createElement('img')
+         image.src = e.image;
+
+         let title = document.createElement('h4')
+         title.innerText = e.name
+         let price = document.createElement('h5')
+         price.innerText = `$${e.price}`
+        
+         let buttonAdd = document.createElement('button')
+         buttonAdd.textContent = '+'
+         buttonAdd.addEventListener('click',function(){
+               add(e.id)
+         })
+
+         let buttonSub = document.createElement('button')
+         buttonSub.textContent = '-'
+         buttonSub.addEventListener('click',function(){
+            sub(e.id)
+         })
+
+         let remove = document.createElement('button')
+         remove.innerText = 'Remove'
+         remove.style.display = 'block'
+         remove.style.margin = 'auto'
+         remove.style.marginTop = '10px'
+
+         remove.addEventListener('click',function(){
+             cartItems.splice(i,1);
+             localStorage.setItem('cart',JSON.stringify(cartItems))
+             let total = calTotal(cartItems);
+             totalId.textContent = `$${total}`
+             display(cartItems);
+         })
+
+         let quant = document.createElement('button')
+         quant.innerText = e.q;
+
+         buttonDiv.append(buttonSub,quant,buttonAdd,remove)
+
+         imageDiv.append(image)
+         textDiv.append(title,price)
+         div.append(imageDiv,textDiv,buttonDiv)
+         left.append(div)
+    })
+}
+
+// function createElement(ele){
+//     return document.createElement(ele)
+// }
+
+function calTotal(cartItems){
+    let sum = 0;
+    for(let i=0;i<cartItems.length;i++){
+        sum+= (cartItems[i].price*cartItems[i].q)
+    }
+    return sum;
+}
+
+function add(id){
+    cartItems = cartItems.map((e)=>{
+         if(e.id===id){
+             e.q+=1
+         }
+         return e;
+    })
+    let total = calTotal(cartItems)
+    totalId.innerText = `$${total}`
+    display(cartItems)
+    localStorage.setItem('cart',JSON.stringify(cartItems))
+    console.log(cartItems);
+}
+
+function sub(id){
+    cartItems = cartItems.map((e)=>{
+        if(e.id===id && e.q > 1){
+            e.q-=1
+        }
+        return e;
+   })
+   let total = calTotal(cartItems)
+   totalId.innerText = `$${total}`
+   display(cartItems)
+   localStorage.setItem('cart',JSON.stringify(cartItems))
+   console.log(cartItems);
+}
---
+++ scripts/massage.js
@@ -0,0 +1,59 @@
+import {data} from './my_data.js';
+let massage = data.filter((e)=>e.category === 'massage')
+for(let i=1;i<=massage.length;i++){
+    massage[i-1].id = i;
+}
+let cartItem = JSON.parse(localStorage.getItem('cart')) || []
+window.onload = display(massage)
+let sort = document.getElementById('sort');
+
+function display(data){
+     document.getElementById('products').innerHTML = null
+     data.map((e)=>{
+         let div = document.createElement('div');
+
+         let image = document.createElement('img')
+         image.src = e.image
+         let title = document.createElement('h3')
+         title.textContent = e.name
+         let price = document.createElement('p')
+         price.textContent = `$${e.price}`
+         let button = document.createElement('button')
+         button.textContent = 'Add To Cart'
+         button.addEventListener('click',function(){
+             if(isPresent(e)){
+                 alert('Product is already in cart')
+             }else{
+                cartItem.push({...e,q:1})
+                localStorage.setItem('cart',JSON.stringify(cartItem))
+             }
+         })
+
+         div.append(image,title,price,button)
+
+         document.getElementById('products').append(div);
+     })
+}
+
+sort.addEventListener('change',function(){
+     let sortBy = sort.value;
+
+     if(sortBy === 'asc'){
+         massage.sort((a,b)=>a.price-b.price)
+         display(massage)
+     }else if(sortBy === 'desc'){
+        massage.sort((a,b)=>b.price-a.price)
+        display(massage)
+     }else{
+        display(massage)
+     }
+})
+
+function isPresent(ele){
+   for(let i=0;i<cartItem.length;i++){
+       if(cartItem[i].id === ele.id){
+           return true;
+       }
+   }
+   return false;
+}
